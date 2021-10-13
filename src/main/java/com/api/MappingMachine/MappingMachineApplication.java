55e8d24fd47797576581ed88abdeecfa88d4efe3
ADNAN KHAN <aka034437@gmail.com>
2021-10-13 11:11:40 +0530

    initial commit

---
+++ src/main/java/com/api/MappingMachine/MappingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.api.MappingMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/MappingMachine/MappingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.MappingMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
